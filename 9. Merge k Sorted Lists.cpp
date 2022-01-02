12af8d09297c2c9c0d98ef74e67b282561e0d4a8
Gookun <75612656+Gookun@users.noreply.github.com>
2022-01-02 16:15:02 +0530

    <done>

---
+++ 9. Merge k Sorted Lists.cpp
@@ -0,0 +1,46 @@
+/*Iterative Merge with Divide And Conquer
+TC = log(n) * (l1 + l2) where n is total number of lists, 
+l1 is average size of list1 and l2 is average size of list2.
+*/
+class Solution {
+public:
+     ListNode *mergeTwoLists(ListNode* l1, ListNode* l2) 
+        {
+            ListNode  dummy(-1); 
+            ListNode *result = &dummy;
+            while(l1 && l2)
+            {
+                if(l1->val <= l2->val)
+                {
+                    result->next = l1;
+                    result = result->next;
+                    l1 = l1->next;
+                }
+                else
+                {
+                    result->next = l2;                
+                    result = result->next;
+                    l2 = l2->next;
+                }            
+            }
+            if(l1)
+                result->next = l1;
+            else if(l2)
+                result->next = l2;
+            return dummy.next;
+        }
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+         if (lists.empty()) return NULL;
+            int len = lists.size();
+            while (len > 1) 
+            {
+                for (int i = 0; i < len / 2; ++i) 
+                {
+                    lists[i] = mergeTwoLists(lists[i], lists[len - 1 - i]);
+                }
+                len = (len + 1) / 2;
+            }
+            return lists.front();
+    }
+};
