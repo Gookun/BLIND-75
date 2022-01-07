ab1d38a1b49ddf98c1699b1f0868804a95849fb0
Gookun <75612656+Gookun@users.noreply.github.com>
2022-01-07 16:39:37 +0530

    <done>

---
+++ 1. Arrays/55. Product of Array Except Self.cpp
@@ -0,0 +1,47 @@
+/*2 passes - first multiply by all elements in the right, 
+then go from the end and multiply by all elements in the left:
+Prefix and postfix
+TC= O(n)
+SC = O(1)*/
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size(), temp = 1;
+        vector<int> res(n);
+        
+        // Put in each index in res the product of all elements before it:
+        for (int i=0; i<n; i++) {
+            res[i] = temp;
+            temp *= nums[i];
+        }
+        
+        // Now multiply each one with all the elements after it:
+        temp = 1;
+        for (int i=n-1; i>=0; i--) {
+            res[i] *= temp;
+            temp *= nums[i];
+        }
+        
+        return res;
+    }
+};
+
+/*This solution leads to a better one -
+1-pass: do it simultaneously*/
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size()-1, left = 1, right = 1;
+        vector<int> res(n+1, 1);
+
+        for (int i=0; i<=n; i++) {
+            res[i] *= left;
+            res[n-i] *= right;
+            left *= nums[i];
+            right *= nums[n-i];
+        }
+        
+        return res;
+    }
+};
